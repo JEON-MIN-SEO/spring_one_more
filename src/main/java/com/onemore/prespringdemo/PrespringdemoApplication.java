e30f8b7ef2b0a69d174ad9a62ee6f220ed76d0c3
JEON-MIN-SEO <jmsparrowk@naver.com>
2024-03-06 21:59:59 +0900

    first

---
+++ src/main/java/com/onemore/prespringdemo/PrespringdemoApplication.java
@@ -0,0 +1,13 @@
+package com.onemore.prespringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrespringdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrespringdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onemore/prespringdemo/PrespringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onemore.prespringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrespringdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
